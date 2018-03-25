6f0c531acd2bfda456432c662002fd8984b22225
Satyam-sopho <Satyam-sopho@users.noreply.github.com>
2018-03-25 13:05:47 +0530

    Add files via upload

--- IEEE-BIT-Mesra-Website-master/resources/js/main.js
+++ IEEE-BIT-Mesra-Website-master/resources/js/main.js
@@ -1,13 +1,4 @@
-function mytoast() {
-    var x = document.getElementById("snackbar")
-    x.className = "show";
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-}
-
 $(document).ready(function(){
-  $('#main-text').hide().delay(400).fadeIn('slow');
-  $('.ieee-logo').hide().delay(400).fadeIn('slow');
-  $('#scroll-down-icon').hide().delay(800).fadeIn('slow');
   $('#container-2').load('../../about-us.html').fadeIn('slow');
   
   //Below code block is for smooth scrolling on clicking of internal links  
@@ -48,16 +39,6 @@ $('a[href*="#"]')
   }
 });
 
-  //Hover effect on the ieee-logo
-  $('.ieee-logo').hover(
-    function(){
-      $(this).animate({width: '+=6.83px'},{duration: 100, queue: false}).animate({height: '+=2px'},{duration: 100, queue: false});
-    },
-    function(){
-      $(this).animate({width: '-=6.83px'},{duration: 100, queue: false}).animate({height: '-=2px'},{duration: 100, queue: false});
-    }
-  );
-
   //when height is greater than 40px then shrink class is added to nav bar
   $(window).scroll(function() {
     if($(document).scrollTop() > 40) {
@@ -74,19 +55,37 @@ $('a[href*="#"]')
 
   var curr_active="#home"; 
   $("#home").click(function(){
-    $("#container-2").hide().load('../../about-us.html').fadeIn(1200);
+    $("#container-2").hide().load('../../about-us.html').fadeIn(600);
     if(curr_active!="#home"){
       $(curr_active).removeClass("active");
+      $(curr_active2).removeClass("active");
       $("#home").addClass("active");
+      $("#home2").addClass("active");
       curr_active="#home";
+      curr_active2="#home2";
     }
-  })
+  });
+  $("#projects").click(function(){
+    $("#container-2").hide().load('../../mega-project.html').fadeIn(10);
+    if(curr_active!="#projects"){
+      $(curr_active).removeClass("active");
+      $(curr_active2).removeClass("active");
+      $("#projects").addClass("active");
+      $("#projects2").addClass("active");
+      curr_active="#projects";
+      curr_active2="#projects2";
+    }
+  });
+
   $("#members").click(function(){
     $('#container-2').hide().load('../../members.html').fadeIn(600);
     if(curr_active!="#members"){
       $(curr_active).removeClass("active");
+      $(curr_active2).removeClass("active");
       $("#members").addClass("active");
+      $("#members2").addClass("active");
       curr_active="#members";
+      curr_active2="#members2";
     }
   });
   $("#about-us").click(function(){
@@ -95,6 +94,9 @@ $('a[href*="#"]')
       $(curr_active).removeClass("active");
       $("#about-us").addClass("active");
       curr_active="#about-us";
+      $(curr_active2).removeClass("active");
+      $("#about-us2").addClass("active");
+      curr_active2="#about-us2";
     }
   });
   $("#event").click(function(){
@@ -103,6 +105,9 @@ $('a[href*="#"]')
       $(curr_active).removeClass("active");
       $("#event").addClass("active");
       curr_active="#event";
+      $(curr_active2).removeClass("active");
+      $("#event2").addClass("active");
+      curr_active2="#event2";
     }
   });
 
@@ -113,6 +118,9 @@ $('a[href*="#"]')
       $(curr_active).removeClass("active");
       $("#pg").addClass("active");
       curr_active="#pg";
+       $(curr_active2).removeClass("active");
+      $("#pg2").addClass("active");
+      curr_active2="#pg2";
     }
   });
   $('#contact-us').click(function(){
@@ -121,24 +129,68 @@ $('a[href*="#"]')
       $(curr_active).removeClass("active");
       $("#contact-us").addClass("active");
       curr_active="#contact-us";
+      $(curr_active2).removeClass("active");
+      $("#contact-us2").addClass("active");
+      curr_active2="#contact-us2";
     }
   });
+  $("#downloads").click(function(){
+    $('#container-2').hide().load('../../downloads.html').fadeIn(600);
+    if(curr_active!="#downloads"){
+      $(curr_active).removeClass("active");
+      $(curr_active2).removeClass("active");
+      $("#downloads").addClass("active");
+      $("#downloads2").addClass("active");
+      curr_active="#downloads";
+      curr_active2="#downloads2";
+    }
+  });
+  $("#downloads-btn").click(function(){
+    $('#container-2').hide().load('../../downloads.html').fadeIn(600);
+    if(curr_active!="#downloads"){
+      $(curr_active).removeClass("active");
+      $(curr_active2).removeClass("active");
+      $("#downloads").addClass("active");
+      $("#downloads2").addClass("active");
+      curr_active="#downloads";
+      curr_active2="#downloads2";
+    }
+  });
+
   
   var curr_active2=curr_active+"2"; 
   $("#home2").click(function(){
-    $("#container-2").hide().load('../../about-us.html').fadeIn(1200);
+    $("#container-2").hide().load('../../about-us.html').fadeIn(600);
     if(curr_active2!="#home2"){
       $(curr_active2).removeClass("active");
       $("#home2").addClass("active");
       curr_active2="#home2";
+      $(curr_active).removeClass("active");
+      $("#home").addClass("active");
+      curr_active="#home";
     }
-  })
+  });
+  $("#projects2").click(function(){
+    $("#container-2").hide().load('../../mega-project.html').fadeIn(10);
+    if(curr_active2!="#projects2"){
+      $(curr_active2).removeClass("active");
+      $("#projects2").addClass("active");
+      curr_active2="#projects2";
+      $(curr_active).removeClass("active");
+      $("#projects").addClass("active");
+      curr_active="#projects";
+    }
+  });
+  
   $("#members2").click(function(){
     $('#container-2').hide().load('../../members.html').fadeIn(600);
     if(curr_active2!="#members2"){
       $(curr_active2).removeClass("active");
       $("#members2").addClass("active");
       curr_active2="#members2";
+      $(curr_active).removeClass("active");
+      $("#members").addClass("active");
+      curr_active="#members";
     }
   });
   $("#about-us2").click(function(){
@@ -147,6 +199,9 @@ $('a[href*="#"]')
       $(curr_active2).removeClass("active");
       $("#about-us2").addClass("active");
       curr_active2="#about-us2";
+      $(curr_active).removeClass("active");
+      $("#about-us").addClass("active");
+      curr_active="#about-us";
     }
   });
   $("#event2").click(function(){
@@ -155,6 +210,9 @@ $('a[href*="#"]')
       $(curr_active2).removeClass("active");
       $("#event2").addClass("active");
       curr_active2="#event2";
+      $(curr_active).removeClass("active");
+      $("#event").addClass("active");
+      curr_active="#event";
     }
   });
 
@@ -165,18 +223,32 @@ $('a[href*="#"]')
       $(curr_active2).removeClass("active");
       $("#pg2").addClass("active");
       curr_active2="#pg2";
+      $(curr_active).removeClass("active");
+      $("#pg").addClass("active");
+      curr_active="#pg";
     }
   });
   $('#contact-us2').click(function(){
-    $('#container-2').hide().load('../../contact-us.html').fadeIn(600);
+    $('#container-2').hide().load('../../contact-us.html').fadeIn(100);
     if(curr_active2!="#contact-us2"){
       $(curr_active2).removeClass("active");
       $("#contact-us2").addClass("active");
       curr_active2="#contact-us2";
+      $(curr_active).removeClass("active");
+      $("#contact-us").addClass("active");
+      curr_active="#contact-us";
     }
   });
-  
-
-  
+  $('#downloads2').click(function(){
+    $('#container-2').hide().load('../../downloads.html').fadeIn(100);
+    if(curr_active2!="#downloads2"){
+      $(curr_active2).removeClass("active");
+      $("#downloads2").addClass("active");
+      curr_active2="#downloads2";
+      $(curr_active).removeClass("active");
+      $("#downloads").addClass("active");
+      curr_active="#downloads";
+    }
+  });
 });
 
---
+++ IEEE-BIT-Mesra-Website-master/resources/js/popup.js
@@ -0,0 +1,11 @@
+function close_popup(){
+    $('.popup').toggle();
+    $('.ieee-logo').toggle();
+    $('#main-text').toggle();
+}
+
+function open_popup(){
+    $('.popup').css("display","block");
+    $('.ieee-logo').toggle();
+    $('#main-text').toggle();
+}
